63244b413cc56e2b1fd9e816f3805e49e8dc0a86
m-shehzad455 <shehzuali5@gmail.com>
2024-01-04 18:26:25 +0500

    update new project dice droller

---
+++ diceroller/index.js
@@ -0,0 +1,17 @@
+function rollDice(){
+
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`);
+    }
+
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
+}
